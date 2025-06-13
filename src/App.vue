bdbd3850300070ac212d9612304cf226a29cd39d
JohnySilverweb <maxermakov444@gmail.com>
2025-06-13 15:51:06 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,145 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div id ='main_container'>
+        <header>
+                <a href="#">Upcoming</a>
+                <a href="#">Legacy</a>
+                <img src="../public/img/logo.png" alt="star wars logo">
+                <a href="#">Merch</a>
+                <a href="#">About</a>
+        </header>
+        <div class="content">
+            <div class="item-1">
+                <div class="img-wrapper">
+                    <img src="../public/img/yoda.jpg" alt="yoda">
+                    <div class="data">
+                        <div class="text">
+                            <p>Name:<span></span></p>
+                            <p>Heigth:<span></span></p>
+                            <p>Mass:<span></span></p>
+                            <p>Hair color:<span></span></p>
+                            <p>Skin color:<span></span></p>
+                            <p>Eye color:<span></span></p>
+                            <p>Birth year:<span></span></p>
+                            <p>Gender:<span></span></p>
+                        </div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item-2">
+                <img src="../public/img/darth_vader.jpg" alt="darth vader">
+                <div class="data">
+                    <div class="text">
+                        <p>Name:<span></span></p>
+                        <p>Heigth:<span></span></p>
+                        <p>Mass:<span></span></p>
+                        <p>Hair color:<span></span></p>
+                        <p>Skin color:<span></span></p>
+                        <p>Eye color:<span></span></p>
+                        <p>Birth year:<span></span></p>
+                        <p>Gender:<span></span></p>
+                    </div>
+                    <button>Edit</button>
+                </div>
+            </div>
+            <div class="item-3">
+                <img src="../public/img/obi_wan.jpg" alt="obi wan">
+                <div class="data">
+                    <div class="text">
+                        <p>Name:<span></span></p>
+                        <p>Heigth:<span></span></p>
+                        <p>Mass:<span></span></p>
+                        <p>Hair color:<span></span></p>
+                        <p>Skin color:<span></span></p>
+                        <p>Eye color:<span></span></p>
+                        <p>Birth year:<span></span></p>
+                        <p>Gender:<span></span></p>
+                    </div>
+                    <button>Edit</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.dislikes += 1;
+        }
+    }
+} 
+</script>
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+    font-family: 'Droid Sans';
+    src: url('../public/fonts/droid-sans/DroidSans-Bold.ttf');
 }
 
-nav {
-  padding: 30px;
+body {
+    background-color: black;
+    font-family: 'Droid Sans';
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+header {
+    margin: 0 26%;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 77px;
+    font-size: 20px;
+    a {
+        text-decoration: none;
+        color: white;
+    }
+    img {
+        width: 210px;
+        height: 127px;
+    }
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.content {
+    padding: 200px;
+    gap: 22px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .item-1,
+    .item-2,
+    .item-3 {
+        width: 283px;
+        height: 614px;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 10px;
+        }
+        .data {
+            width: 240px;
+            height: 306px;
+            color: white;
+            gap: 38px;
+            .text {
+                margin: 0;
+                gap: 8px;
+                font-size: 18px;
+            }
+        }
+    }
 }
 </style>
