6da372adcf68951d1f1726eb1ad87f7cfccfd2d0
JohnySilverweb <maxermakov444@gmail.com>
2025-06-16 10:58:53 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -8,54 +8,49 @@
                 <a href="#">About</a>
         </header>
         <div class="content">
-            <div class="item-1">
-                <div class="img-wrapper">
-                    <img src="../public/img/yoda.jpg" alt="yoda">
-                    <div class="data">
-                        <div class="text">
-                            <p>Name:<span></span></p>
-                            <p>Heigth:<span></span></p>
-                            <p>Mass:<span></span></p>
-                            <p>Hair color:<span></span></p>
-                            <p>Skin color:<span></span></p>
-                            <p>Eye color:<span></span></p>
-                            <p>Birth year:<span></span></p>
-                            <p>Gender:<span></span></p>
-                        </div>
-                        <button>Edit</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item-2">
-                <img src="../public/img/darth_vader.jpg" alt="darth vader">
+            <div class="item item-1">
                 <div class="data">
                     <div class="text">
-                        <p>Name:<span></span></p>
-                        <p>Heigth:<span></span></p>
-                        <p>Mass:<span></span></p>
-                        <p>Hair color:<span></span></p>
-                        <p>Skin color:<span></span></p>
-                        <p>Eye color:<span></span></p>
-                        <p>Birth year:<span></span></p>
-                        <p>Gender:<span></span></p>
+                        <p>Name: <span>Yoda</span></p>
+                        <p>Height: <span>66 cm</span></p>
+                        <p>Mass: <span>13 kg</span></p>
+                        <p>Hair color: <span>White</span></p>
+                        <p>Skin color: <span>Green</span></p>
+                        <p>Eye color: <span>Green-gold</span></p>
+                        <p>Birth year: <span>896 BBY</span></p>
+                        <p>Gender: <span>Male</span></p>
                     </div>
-                    <button>Edit</button>
+                    <button><p>Edit</p></button>
                 </div>
             </div>
-            <div class="item-3">
-                <img src="../public/img/obi_wan.jpg" alt="obi wan">
+            <div class="item item-2">
                 <div class="data">
                     <div class="text">
-                        <p>Name:<span></span></p>
-                        <p>Heigth:<span></span></p>
-                        <p>Mass:<span></span></p>
-                        <p>Hair color:<span></span></p>
-                        <p>Skin color:<span></span></p>
-                        <p>Eye color:<span></span></p>
-                        <p>Birth year:<span></span></p>
-                        <p>Gender:<span></span></p>
+                        <p>Name: <span>Darth Vader</span></p>
+                        <p>Height: <span>203 cm</span></p>
+                        <p>Mass: <span>120 kg (with armor)</span></p>
+                        <p>Hair color: <span>Dark</span></p>
+                        <p>Skin color: <span>Pale</span></p>
+                        <p>Eye color: <span>Yellow</span></p>
+                        <p>Birth year: <span>41 BBY</span></p>
+                        <p>Gender: <span>Male</span></p>
                     </div>
-                    <button>Edit</button>
+                    <button><p>Edit</p></button>
+                </div>
+            </div>
+            <div class="item item-3">
+                <div class="data">
+                    <div class="text">
+                        <p>Name: <span>Obi-wan Kenobi</span></p>
+                        <p>Height: <span>182 cm</span></p>
+                        <p>Mass: <span>81 kg</span></p>
+                        <p>Hair color: <span>Auburn</span></p>
+                        <p>Skin color: <span>Light</span></p>
+                        <p>Eye color: <span>Gray-blue</span></p>
+                        <p>Birth year: <span>57 BBY</span></p>
+                        <p>Gender: <span>Male</span></p>
+                    </div>
+                    <button><p>Edit</p></button>
                 </div>
             </div>
         </div>
@@ -64,35 +59,25 @@
 
 
 <script>
-export default {
-    data() {
-        return {
-            likes: 0,
-            dislikes: 0,
-        }
-    },
-    methods: {
-        addLike() {
-            this.likes += 1;
-        },
-        addDislike() {
-            this.dislikes += 1;
-        }
-    }
-} 
+
 </script>
 
 
 
 <style>
 @font-face {
-    font-family: 'Droid Sans';
+    font-family: 'Droid Sans Bold';
     src: url('../public/fonts/droid-sans/DroidSans-Bold.ttf');
+}
+
+@font-face {
+    font-family: 'Droid Sans';
+    src: url('../public/fonts/droid-sans/DroidSans.ttf');
 }
 
 body {
     background-color: black;
-    font-family: 'Droid Sans';
+    font-family: Droid Sans Bold;
 }
 
 header {
@@ -119,27 +104,62 @@ header {
     display: flex;
     justify-content: center;
     align-items: center;
-    .item-1,
-    .item-2,
-    .item-3 {
+    .item {
         width: 283px;
         height: 614px;
-        img {
-            width: 100%;
-            height: 100%;
-            border-radius: 10px;
-        }
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        box-shadow: 0px 0px 250px 0px #E3D61D66;
         .data {
-            width: 240px;
-            height: 306px;
+            margin: 33px 20px;
+            display: flex;
+            flex-direction: column;
             color: white;
             gap: 38px;
             .text {
-                margin: 0;
-                gap: 8px;
                 font-size: 18px;
+                p {
+                    font-family: Droid Sans;
+                    margin: 8px;
+                }
+                span {
+                    font-family: Droid Sans Bold;
+                }
+            }
+
+            button {
+                width: 146px;
+                height: 44px;
+                border-radius: 7px;
+                padding: 8px 50px;
+                border: none;
+                background: #E3D61D;
+                box-shadow: 0px 0px 30px 0px #E3D61D4D;
+                align-self: center;
+                cursor: pointer;
+                p {
+                    font-size: 24px;
+                    font-weight: 700;
+                    margin: 0;
+                }
             }
         }
+    }
+    .item-1 {
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0) 29.41%, rgba(0, 0, 0, 0.8) 62.6%),
+        url('../public/img/yoda.jpg') center / cover no-repeat;
+    }
+
+    .item-2 {
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0) 29.41%, rgba(0, 0, 0, 0.8) 62.6%),
+        url('../public/img/darth_vader.jpg') center / cover no-repeat;
+    }
+
+    .item-3 {
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0) 29.41%, rgba(0, 0, 0, 0.8) 62.6%),
+        url('../public/img/obi_wan.jpg') center / cover no-repeat;
     }
 }
 </style>
